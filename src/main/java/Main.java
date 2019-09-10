20ab8842f3dd39c42bdf9cc71d130fbcc58ae2fc
JasperGJ <jasperstel@gmail.com>
2019-09-10 10:24:44 +0200

    volledige zin

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,7 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.printl("Hallo Jasper");
+        System.out.println("Hallo ik ben Jasper");
     }
     
 }
